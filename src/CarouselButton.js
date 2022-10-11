39c1ba90820751171d86968bce6e569d8d0ae24d
Tim McMahon <Tim.McMahon@jbtc.com>
2022-10-11 11:37:36 -0500

    initial carousel button implementation

--- src/CarouselButton.js
+++ src/CarouselButton.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CarouselButton = () => {
-  return <button />;
+const CarouselButton = (props) => {
+  return <button {...props} />;
+};
+
+CarouselButton.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default CarouselButton;
